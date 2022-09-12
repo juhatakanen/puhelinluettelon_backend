515dc5f2e2cd79f38f802222d5613944a9bf35ec
Juha Takanen <juhatakanen@hotmail.com>
2022-09-12 13:55:05 +0300

    3.8* tehty

--- index.js
+++ index.js
@@ -3,7 +3,20 @@ const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
-app.use(morgan('tiny'))
+app.use(morgan(function (tokens, req, res) {
+    return [
+        tokens.method(req, res),
+        tokens.url(req, res),
+        tokens.status(req, res),
+        tokens.res(req, res, 'content-length'), '-',
+        tokens['response-time'](req, res), 'ms',
+        tokens.postData(req,res)
+    ].join(' ')
+}))
+morgan.token('postData', function (req, res) { if(JSON.stringify(req.body) !== '{}') {
+    return JSON.stringify(req.body)
+    }
+ })
 
 let persons = [
     {
