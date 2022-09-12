7640122110828a8a8a47a5c48836822d975a9b0a
Juha Takanen <juhatakanen@hotmail.com>
2022-09-12 15:10:12 +0300

    Heroku-testi

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
+const cors = require('cors')
 
+app.use(cors())
 app.use(express.json())
 app.use(morgan(function (tokens, req, res) {
     return [
@@ -104,7 +106,7 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end()
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
