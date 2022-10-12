70f2264a914daadcee22cc58278bba922f9f1147
Juha Takanen <juhatakanen@hotmail.com>
2022-10-12 13:10:45 +0300

    3.19-3.12 tehty

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ app.get('/api/persons/:id', (request, response, next) => {
 //     return Math.round(Math.random() * 100000000)
 //   }
   
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
   if (!body.name) {
@@ -105,6 +105,7 @@ app.post('/api/persons', (request, response) => {
   person.save().then(savedPerson => {
     response.json(savedPerson)
   })
+  .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
@@ -143,8 +144,9 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
-
   next(error)
 }
 
